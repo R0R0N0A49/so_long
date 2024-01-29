0d18158cad3733e141a6ee18976607b4073f1a3a
R0R0N0A49 <150067904+R0R0N0A49@users.noreply.github.com>
2024-01-29 17:50:45 +0100

    Add files via upload

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,14 +6,11 @@
 /*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:39:41 by trebours          #+#    #+#             */
-/*   Updated: 2024/01/27 17:42:43 by trebours         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:39:42 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include "MLX42/include/MLX42/MLX42.h"
-#define WIDTH 512
-#define HEIGHT 512
 
 void	ft_free_map(char **map)
 {
@@ -35,48 +32,223 @@ void	ft_error(t_maps *parsing, char *error)
 	exit(1);
 }
 
+char	ft_corner_high(t_maps *parsing, int i, size_t j)
+{
+	if (j < parsing->len_line_map - 2 && i < parsing->len_map - 1
+		&& parsing->map[i][j] == '1' && parsing->map[i][j + 1] == '1'
+		&& parsing->map[i + 1][j] == '1')
+	{
+		if ((j == 0 && i == 0) || (j > 0 && parsing->map[i][j - 1] != '1'
+			&& i == 0) || (j > 0 && parsing->map[i][j - 1] != '1' && i > 0
+			&& parsing->map[i - 1][j] != '1') || (j == 0 && i > 0
+			&& parsing->map[i - 1][j] != '1'))
+			return ('o');
+	}
+	else if (j > 0 && i < parsing->len_map - 1
+		&& parsing->map[i][j] == '1' && parsing->map[i][j - 1] == '1'
+		&& parsing->map[i + 1][j] == '1')
+	{
+		if ((j == parsing->len_line_map - 2 && i == 0) || (j
+				< parsing->len_line_map - 2 && parsing->map[i][j + 1] != '1'
+			&& i == 0) || (j < parsing->len_line_map - 2
+			&& parsing->map[i][j + 1] != '1' && i > 0
+			&& parsing->map[i - 1][j] != '1') || (j == parsing->len_line_map - 2 && i > 0
+			&& parsing->map[i - 1][j] != '1'))
+			return ('p');
+	}
+	return ('1');
+}
+
+char	ft_corner_botom(t_maps *parsing, int i, size_t j, char c)
+{
+	if (j < parsing->len_line_map - 2 && i > 0 && parsing->map[i][j] == c
+		&& parsing->map[i][j + 1] == c && parsing->map[i - 1][j] == c)
+	{
+		if ((j == 0 && i == parsing->len_map - 1) || (j > 0
+			&& parsing->map[i][j - 1] != c && i == parsing->len_map - 1) || (j
+			> 0 && parsing->map[i][j - 1] != c && i < parsing->len_map - 1
+			&& parsing->map[i + 1][j] != c) || (j == 0 && i < parsing->len_map
+			- 1 && parsing->map[i + 1][j] != c))
+			return ('m');
+	}
+	else if (j > 0 && i > 0 && parsing->map[i][j] == c
+		&& parsing->map[i][j - 1] == c && parsing->map[i - 1][j] == c)
+	{
+		if ((j == parsing->len_line_map - 2 && i == parsing->len_map - 1)
+			|| (j < parsing->len_line_map - 2 && parsing->map[i][j + 1] != c
+			&& i == parsing->len_map - 1) || (j < parsing->len_line_map - 2
+			&& parsing->map[i][j + 1] != c && i < parsing->len_map - 1
+			&& parsing->map[i + 1][j] != c) || (j == parsing->len_line_map - 2
+			&& i < parsing->len_map - 1
+			&& parsing->map[i + 1][j] != c))
+			return ('n');
+	}
+	return ('1');
+}
+
+char	ft_line(t_maps *parsing, int i, size_t j, char c)
+{
+	if (parsing->map[i][j] == c && ((j > parsing->len_line_map - 2 && parsing->map[i][j + 1] == c) || (j > 0 && parsing->map[i][j - 1] == c)))
+		return ('b');
+	else if (parsing->map[i][j] == c && (j < parsing->len_line_map - 2 && parsing->map[i][j + 1] != c)
+		&& (j > 0 && parsing->map[i][j - 1] != c) && (i < parsing->len_map - 1 && parsing->map[i + 1][j] != c)
+		&& (i > 0 && parsing->map[i - 1][j] != c))
+		return ('c');
+	return ('a');
+}
+
+char	choise_char(t_maps *parsing, int i, size_t j)
+{
+	char	rsl;
+	const char	c = '1';
+
+	rsl = ft_corner_high(parsing, i, j);
+	if (rsl == '1')
+		rsl = ft_corner_botom(parsing, i, j, c);
+	if (rsl == '1')
+		rsl = ft_line(parsing, i, j, c);
+	return (rsl);
+}
+
+char	*ft_malloc_line(t_maps *parsing, int i)
+{
+	char	*result;
+	size_t	j;
+
+	result = malloc(parsing->len_line_map * sizeof(char));
+	j = 0;
+	while (parsing->map[i][j] != '\n')
+	{
+		if (parsing->map[i][j] == '1')
+			result[j] = choise_char(parsing, i, j);
+		else
+			result[j] = parsing->map[i][j];
+		j++;
+	}
+	result[j] = 0;
+	return (result);
+}
+
+//cree une nouvelle map en remplasent les 1 par d'autre nombre
+void	final_map(t_maps *parsing)
+{
+	char	**map_final;
+	int		i;
+
+	if (parsing->map[1][1] == '1')
+		parsing->map[0][0] = '0';
+	if (parsing->map[1][parsing->len_line_map - 3] == '1')
+		parsing->map[0][parsing->len_line_map - 2] = '0';
+	if (parsing->map[parsing->len_map - 2][1] == '1')
+		parsing->map[parsing->len_map - 1][0] = '0';
+	if (parsing->map[parsing->len_map - 2][parsing->len_line_map - 3] == '1')
+		parsing->map[parsing->len_map - 1][parsing->len_line_map - 2] = '0';
+	map_final = malloc((parsing->len_map + 1) * sizeof(char *));
+	if (!map_final)
+		ft_error(parsing, "error malloc");
+	i = 0;
+	while (i < parsing->len_map)
+	{
+		map_final[i] = ft_malloc_line(parsing, i);
+		i++;
+	}
+	map_final[i] = NULL;
+	ft_free_map(parsing->map);
+	parsing->map = map_final;
+}
+
 static void error(void)
 {
 	ft_printf("%s", (mlx_strerror(mlx_errno)));
 	exit(EXIT_FAILURE);
 }
 
-int32_t	ft_test_mlx(void)
+int32_t	ft_test_mlx(t_maps *map)
 {
 	// Start mlx
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Test", false);
+	int	i;
+	int	j;
+	mlx_t* mlx = mlx_init((map->len_line_map - 1) * 32, (map->len_map) * 32, "Test", false);
 	if (!mlx)
         error();
 
 	// Try to load the file
-	mlx_texture_t* texture = mlx_load_png("../tile_so_long/wall_horizontal.png");
-	mlx_texture_t* texture_1 = mlx_load_png("../tile_so_long/wall_vertical.png");
-	mlx_texture_t* texture_2 = mlx_load_png("../tile_so_long/corner_high_left.png");
-	mlx_texture_t* texture_3 = mlx_load_png("../tile_so_long/floor.png");
-	
-	if (!texture)
-        error();
+	mlx_texture_t* texture_a = mlx_load_png("../tile_so_long/wall_vertical.png");
+	mlx_texture_t* texture_b = mlx_load_png("../tile_so_long/wall_horizontal.png");
+	mlx_texture_t* texture_c = mlx_load_png("../tile_so_long/wall_alone.png");
+	mlx_texture_t* texture_o = mlx_load_png("../tile_so_long/corner_high_left.png");
+	mlx_texture_t* texture_p = mlx_load_png("../tile_so_long/corner_high_right.png");
+	mlx_texture_t* texture_m = mlx_load_png("../tile_so_long/corner_botom_left.png");
+	mlx_texture_t* texture_n = mlx_load_png("../tile_so_long/corner_botom_right.png");
+	mlx_texture_t* texture_0 = mlx_load_png("../tile_so_long/floor.png");
+	mlx_texture_t* texture_P = mlx_load_png("../tile_so_long/mouse_left.png");
+	mlx_texture_t* texture_E = mlx_load_png("../tile_so_long/banana.png");
+	mlx_texture_t* texture_C = mlx_load_png("../tile_so_long/apple.png");
+		
 	
 	// Convert texture to a displayable image
-	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
-	mlx_image_t* img_1 = mlx_texture_to_image(mlx, texture_1);
-	mlx_image_t* img_2 = mlx_texture_to_image(mlx, texture_2);
-	mlx_image_t* img_3 = mlx_texture_to_image(mlx, texture_3);
-	if (!img)
-        error();
+	mlx_image_t* img_a = mlx_texture_to_image(mlx, texture_a);
+	mlx_image_t* img_b = mlx_texture_to_image(mlx, texture_b);
+	mlx_image_t* img_c = mlx_texture_to_image(mlx, texture_c);
+	mlx_image_t* img_o = mlx_texture_to_image(mlx, texture_o);
+	mlx_image_t* img_p = mlx_texture_to_image(mlx, texture_p);
+	mlx_image_t* img_m = mlx_texture_to_image(mlx, texture_m);
+	mlx_image_t* img_n = mlx_texture_to_image(mlx, texture_n);
+	mlx_image_t* img_0 = mlx_texture_to_image(mlx, texture_0);
+	mlx_image_t* img_E = mlx_texture_to_image(mlx, texture_E);
+	mlx_image_t* img_C = mlx_texture_to_image(mlx, texture_C);
+	mlx_image_t* img_P = mlx_texture_to_image(mlx, texture_P);
 
 	// Display the image
-	mlx_image_to_window(mlx, img_1, 0, 32);
-	mlx_image_to_window(mlx, img_2, 0, 0);
-	mlx_image_to_window(mlx, img, 32, 0);
-	mlx_image_to_window(mlx, img_3, 32, 32);
-
+	i = 0;
+	while (map->map[i])
+	{
+		j = 0;
+		while (map->map[i][j])
+		{
+			if (map->map[i][j] == 'a')
+				mlx_image_to_window(mlx, img_a, (j*32), (i*32));
+			else if (map->map[i][j] == 'b')
+				mlx_image_to_window(mlx, img_b, (j*32), (i*32));
+			else if (map->map[i][j] == 'c')
+				mlx_image_to_window(mlx, img_c, (j*32), (i*32));
+			else if (map->map[i][j] == 'o')
+				mlx_image_to_window(mlx, img_o, (j*32), (i*32));
+			else if (map->map[i][j] == 'p')
+				mlx_image_to_window(mlx, img_p, (j*32), (i*32));
+			else if (map->map[i][j] == 'm')
+				mlx_image_to_window(mlx, img_m, (j*32), (i*32));
+			else if (map->map[i][j] == 'n')
+				mlx_image_to_window(mlx, img_n, (j*32), (i*32));
+			else if (map->map[i][j] == '0' || map->map[i][j] == 'P' || map->map[i][j] == 'E' || map->map[i][j] == 'C')
+				mlx_image_to_window(mlx, img_0, (j*32), (i*32));
+			if (map->map[i][j] == 'P')
+				mlx_image_to_window(mlx, img_P, (j*32), (i*32));
+			else if (map->map[i][j] == 'E')
+				mlx_image_to_window(mlx, img_E, (j*32), (i*32));
+			else if (map->map[i][j] == 'C')
+				mlx_image_to_window(mlx, img_C, (j*32), (i*32));
+			j++;
+		}
+		i++;
+	}
 
 	mlx_loop(mlx);
 
 	// Optional, terminate will clean up any leftovers, this is just to demonstrate.
-	mlx_delete_image(mlx, img);
-	mlx_delete_texture(texture);
+	mlx_delete_image(mlx, img_a);
+	mlx_delete_image(mlx, img_b);
+	mlx_delete_image(mlx, img_m);
+	mlx_delete_image(mlx, img_p);
+	mlx_delete_image(mlx, img_o);
+	mlx_delete_image(mlx, img_0);
+
+	mlx_delete_texture(texture_a);
+	mlx_delete_texture(texture_b);
+	mlx_delete_texture(texture_o);
+	mlx_delete_texture(texture_p);
+	mlx_delete_texture(texture_m);
+	mlx_delete_texture(texture_0);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
@@ -97,7 +269,15 @@ int	main(int argc, char **argv)
 	verif_quote(&parsing);
 	verif_char(&parsing);
 	ft_is_finished(&parsing);
-	ft_test_mlx();
+	final_map(&parsing);
+	int	j = 0;
+	while (parsing.map[j])
+	{
+		ft_printf("%s\n", parsing.map[j]);
+		j++;
+	}
+	
+	ft_test_mlx(&parsing);
 	ft_free_map(parsing.map);
 	return (0);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: trebours <trebours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 09:17:33 by trebours          #+#    #+#             */
-/*   Updated: 2024/01/27 15:56:12 by trebours         ###   ########.fr       */
+/*   Updated: 2024/01/29 11:04:05 by trebours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SO_LONG_H
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
+# include "MLX42/include/MLX42/MLX42.h"
 
 typedef struct s_maps
 {
@@ -25,6 +26,26 @@ typedef struct s_maps
 	int		x;
 	int		y;
 }t_maps;
+
+typedef struct s_wall
+{
+	mlx_image_t	*wall_a; //wall_vertical.png
+	mlx_image_t	*wall_b; //wall_horizontal.png
+	mlx_image_t	*wall_c; //wall_alone.png
+	mlx_image_t	*wall_d; //cross_wall.png
+	mlx_image_t	*wall_e; //t_botom.png
+	mlx_image_t	*wall_f; //t_high.png
+	mlx_image_t	*wall_g; //t_left.png
+	mlx_image_t	*wall_h; //t_right.png
+	mlx_image_t	*wall_i; //end_wall_botom.png
+	mlx_image_t	*wall_j; //end_wall_high.png
+	mlx_image_t	*wall_k; //end_wall_left.png
+	mlx_image_t	*wall_l; //end_wall_right.png
+	mlx_image_t	*wall_m; //corner_botom_left.png
+	mlx_image_t	*wall_n; //corner_botom_right.png
+	mlx_image_t	*wall_o; //corner_high_left.png
+	mlx_image_t	*wall_p; //corner_high_right.png
+}t_wall;
 
 void	ft_verif_ber(char *src);
 void	ft_free_map(char **map);
